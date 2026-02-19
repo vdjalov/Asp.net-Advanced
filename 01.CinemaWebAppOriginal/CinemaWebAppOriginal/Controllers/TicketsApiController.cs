cd77aa625c10879be454de01b4264db8ea124393
admin <vdjalov@gmialcom>
2026-02-19 16:55:24 +0200

    Api Managing tickets Initials Added

--- 01.CinemaWebAppOriginal/CinemaWebAppOriginal/Controllers/TicketsApiController.cs
+++ 01.CinemaWebAppOriginal/CinemaWebAppOriginal/Controllers/TicketsApiController.cs
@@ -1,4 +1,6 @@
 ﻿using CinemaWebAppOriginal.Services.Data.Interfaces;
+using CinemaWebAppOriginal.ViewModels;
+using CinemaWebAppOriginal.ViewModels.Ticket;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -8,7 +10,7 @@ namespace CinemaWebAppOriginal.Controllers
     [Route("api/[controller]")]
     [ApiController]
     [Authorize]
-    public class TicketsApiController : ControllerBase
+    public class TicketsApiController : BaseController
     {
         private readonly ITicketService ticketService;
         private readonly ICinemaService cinemaService;
@@ -23,8 +25,44 @@ namespace CinemaWebAppOriginal.Controllers
 
 
 
+        [HttpGet("GetMoviesByCinema/{cinemaId}")]
+        public async Task<IActionResult> GetMoviesByCinema(int cinemaId)
+        {
+            string userId = this.GetUserId();
+            bool isUserManager = await this.managerService.IsUserAManager(userId);
+            if (!isUserManager)
+            {
+                return Unauthorized("Only Managers can access this endpoint.");
+            }
 
+            CinemaProgramViewModel movies = await this.cinemaService.GetCinemaProgramByIdAsync(cinemaId);
 
+            return Ok(movies);
+        }
 
+        [HttpPost("UpdateAvailableTickets")]
+        public async Task<IActionResult> UpdateAvailableTickets([FromBody] SetAvailableTicketsViewModel model)
+        {
+            string userId = this.GetUserId();
+            bool isUserManager = await this.managerService.IsUserAManager(userId);
+            if (!isUserManager)
+            {
+                return Unauthorized("Only Managers can access this endpoint.");
+            }
+
+            if(ModelState.IsValid == false)
+            {
+                return BadRequest("Invalid data. Please ensure all required fields are provided and valid.");
+            }
+           
+
+            bool result = await this.ticketService.SetAvailableTicketsAsync(model.CinemaId, model.MovieId, model.AvailableTickets);
+            if (!result)
+            {
+                return BadRequest("Failed to update available tickets. Please check the provided cinema and movie IDs.");
+            }
+
+            return Ok("Available tickets updated successfully.");
+        }
     }
 }

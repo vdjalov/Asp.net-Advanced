f7f12ed7d69fd6f987a53f0439177755c6ab2e11
admin <vdjalov@gmialcom>
2026-03-19 14:46:55 +0200

    simple chat completed

--- 03. Exercise SignalR/SignalR/SignalR/Program.cs
+++ 03. Exercise SignalR/SignalR/SignalR/Program.cs
@@ -1,3 +1,5 @@
+using SignalR.Hubs;
+
 namespace SignalR
 {
     public class Program
@@ -8,9 +10,11 @@ namespace SignalR
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+            builder.Services.AddSignalR();
 
             var app = builder.Build();
 
+            
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
@@ -18,6 +22,8 @@ namespace SignalR
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            
 
             app.UseHttpsRedirection();
             app.UseRouting();
@@ -29,6 +35,8 @@ namespace SignalR
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}")
                 .WithStaticAssets();
+
+            app.MapHub<ChatHub>("/chatHub");
 
             app.Run();
         }
